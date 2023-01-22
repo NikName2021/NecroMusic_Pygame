905518a4d42a06b46bef9bc15aec144a7efbda84
d-mega <51013505+d-mega@users.noreply.github.com>
2023-01-22 10:02:45 +0300

    Вторая музыка

--- Sound.py
+++ Sound.py
@@ -1,10 +1,11 @@
 import pygame
+import random
 
 
 class Sound:
     def __init__(self):
         pygame.init()
-        pygame.mixer.music.load('layouts/necrotic_music.mp3')
+        pygame.mixer.music.load(random.choice(['layouts/necrotic_music.mp3', 'layouts/necrotic_music2.mp3']))
         pygame.mixer.music.play()
         pygame.mixer.music.set_volume(0.5)
 
